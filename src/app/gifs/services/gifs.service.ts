93a3db2a2ff5dfe1fda57eff34496ff598734a71
Esteban Garcia <halo.3.a@hotmail.es>
2022-11-30 22:59:34 -0600

    Termino de la aplicacion /Con las utimas actualizaciones subidas por este medio/ #5/

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, LOCALE_ID } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Gif, SearchGifsReponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -7,8 +7,9 @@ import { Gif, SearchGifsReponse } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  private apikey: string = '5f4GK2Iar6FAWapXfGnZoZ7CJ1jrY13c';
-  private _historial: string[] = [];
+  private apikey     : string = '5f4GK2Iar6FAWapXfGnZoZ7CJ1jrY13c';
+  private servicioUrl: string = 'http://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
   public resultados: Gif[] = [];
 
@@ -20,6 +21,7 @@ export class GifsService {
   constructor( private http: HttpClient) {
 
    this._historial = JSON.parse(localStorage.getItem('historial')! ) || [];
+   this.resultados = JSON.parse(localStorage.getItem('resultados')! ) || [];
   /*  if(localStorage.getItem('historial')){
     this._historial = JSON.parse(localStorage.getItem('historial')! );
    } */
@@ -38,12 +40,19 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get<SearchGifsReponse>(`http://api.giphy.com/v1/gifs/search?api_key=5f4GK2Iar6FAWapXfGnZoZ7CJ1jrY13c&q=${query}&limit=10`).
+    const params = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http.get<SearchGifsReponse>(`${this.servicioUrl}/search`, { params } ).
      subscribe((resp) => {
-       console.log(resp.data);
+       /* console.log(resp.data); */
        this.resultados = resp.data;
+
        
-     });
+       localStorage.setItem('resultados', JSON.stringify(this.resultados) );
+      });
     
   }
   /* fetch('http://api.giphy.com/v1/gifs/search?api_key=5f4GK2Iar6FAWapXfGnZoZ7CJ1jrY13c&q=gears&limit=10')
